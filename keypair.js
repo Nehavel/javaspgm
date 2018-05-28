ce40fac1dbfa3925fd5dd522c8922750c0a49e29
Neha Velmurugan <nvelmurugan@virtusa.com>
2018-05-28 16:17:27 +0530

    first commit

---
+++ keypair.js
@@ -0,0 +1,9 @@
+
+function findsum(){
+    var text=document.getElementById("hi").value;
+    var sum=0;
+    var jh=JSON.parse(text,function(key,value){
+        sum=sum+value;
+});
+    alert("sum of values " + parseInt(sum));
+}
